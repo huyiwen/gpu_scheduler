e9a697a690691f99839206d0c502f4c64cd54586
huyiwen <1020030101@qq.com>
2023-11-09 01:53:40 +0800

    Use callbacks to save stdout & stderr

--- gpu_scheduler.py
+++ gpu_scheduler.py
@@ -1,14 +1,23 @@
 import subprocess
 import time
+import os
+import signal
+import threading
 from collections import defaultdict
-from typing import List, Tuple, Dict
+from typing import List, Tuple, Dict, Optional
+from logging import getLogger
+from datetime import datetime
 
 from nvitop import Device
+import coloredlogs
+
+coloredlogs.install("INFO")
 
 
 class GPUScheduler:
 
     time_interval = 10
+    logs_dir = "logs"
 
     def __init__(
         self,
@@ -23,17 +32,20 @@ class GPUScheduler:
         else:
             self.max_gpu_count = max_gpu_count
 
-        self.gpus_used_by_proc: Dict[int, Tuple[subprocess.Popen, List[int]],  float] = dict()
+        self.gpus_used_by_proc: Dict[int, Tuple[subprocess.Popen, List[int], str, float]] = dict()
         self.estimated_memory_usage_per_gpu: Dict[int, float] = defaultdict(float)
         self.tasks = []
 
+        if not os.path.exists(self.logs_dir):
+            os.makedirs(self.logs_dir, exist_ok=True)
+        self.logger = getLogger(__name__)
         assert 1 <= self.min_gpu_count <= len(self.devices), f"min_gpu_count should be less than {len(self.devices)}"
 
     @property
     def _used_gpu_count(self):
         """Get the amount of GPU in use. Duplicate GPU will be counted only once."""
         gpus = set()
-        for _, devices_list, _ in self.gpus_used_by_proc.values():
+        for _, devices_list, _, _ in self.gpus_used_by_proc.values():
             gpus.update(devices_list)
         return len(gpus)
 
@@ -45,85 +57,143 @@ class GPUScheduler:
         """
         while True:
             available_gpus = [int(d.cuda_index) for d in self.devices if d.memory_free() >= min_mem + self.estimated_memory_usage_per_gpu[d.cuda_index]]
-            self._wait_procs(False)
+            self._wait_usable_devices()
 
             if len(available_gpus) >= self.min_gpu_count:
                 return available_gpus[:self.min_gpu_count]
             else:
                 time.sleep(self.time_interval)
 
-    def _wait_procs(self, wait_all: bool):
-        """Wait all processes to finish or wait maximum used gpu."""
-        if wait_all:
-            wait_condition = lambda self: len(self.gpus_used_by_proc) == 0
+    def _join_procs(self):
+        """Wait all processes to finish"""
+        while len(self.gpus_used_by_proc) > 0:
+            time.sleep(self.time_interval)
+
+    def _wait_usable_devices(self):
+        """Wait maximum used gpu."""
+        while self._used_gpu_count + self.min_gpu_count > self.max_gpu_count:
+            time.sleep(self.time_interval)
+
+    def _wait_when_error(self):
+        try:
+            signal.alarm(10)
+            kill = input("KeyboardInterrupt: Input 'k' to kill all processes: ") == "k"
+        except (Exception, KeyboardInterrupt):
+            kill = False
+        print()
+        if kill:
+            for  p, _, _, _ in self.gpus_used_by_proc.values():
+                self.logger.info(f"Killing {p.pid}")
+                p.kill()
         else:
-            wait_condition = lambda self: self._used_gpu_count + self.min_gpu_count <= self.max_gpu_count
+            self.logger.warning(" Waiting for all processes to finish...")
+            for p, _, _, _ in self.gpus_used_by_proc.values():
+                p.wait()
 
-        while True:
-            flag = True
-            # print(wait_all, wait_condition(self))
-            for pid, (proc, devices_list, estimated_memory_usage) in self.gpus_used_by_proc.items():
-                # print(proc, devices_list, estimated_memory_usage)
-                if proc.poll() is not None:
-                    self._unset_task_gpu(pid)
-                    flag = False
-                    print("Finished:", proc.args)
-                    print(proc.stdout.read().decode('utf-8'))
-                    break  # break to avoid dictionary changed size during iteration
-            if wait_condition(self):
-                return
-            if flag:
-                time.sleep(10)
-
-    def _set_task_gpu(self, proc: subprocess.Popen, devices_list: List[int], estimated_memory_usage: float):
+    def _set_task_gpu(self, proc: subprocess.Popen, devices_list: List[int], task_name: str, estimated_memory_usage: float):
         """Set the GPU used by the process."""
-        self.gpus_used_by_proc[proc.pid] = (proc, devices_list, estimated_memory_usage)
+        self.gpus_used_by_proc[proc.pid] = (proc, devices_list, task_name, estimated_memory_usage)
         for d in devices_list:
             self.estimated_memory_usage_per_gpu[d] += estimated_memory_usage
 
-    def _unset_task_gpu(self, pid: int):
+    def _unset_task_gpu(self, proc: subprocess.Popen):
         """Unset the GPU used by the process."""
-        _, devices_list, estimated_memory_usage = self.gpus_used_by_proc[pid]
+        pid = proc.pid
+        _, devices_list, task_name, estimated_memory_usage = self.gpus_used_by_proc[pid]
         for d in devices_list:
             self.estimated_memory_usage_per_gpu[d] -= estimated_memory_usage
         self.gpus_used_by_proc.pop(pid)
+        self.logger.info(f"[{task_name}] {pid} Finished: {proc.args}")
 
-    def add(
+        str_stdout = proc.stdout.read().decode('utf-8')
+        with open(f"{self.logs_dir}/{task_name}_{pid}.log", "w") as f:
+            f.write(str_stdout)
+        self.logger.debug(str_stdout)
+
+        str_stderr = proc.stderr.read().decode('utf-8')
+        if len(str_stderr) > 0:
+            with open(f"{self.logs_dir}/{task_name}_{pid}_stderr.log", "w") as f:
+                f.write(str_stderr)
+            self.logger.warning(str_stderr)
+
+    def schedule(
         self,
         cmd_command: List[str],
-        min_mem_in_mib: float,
-        estimated_memory_usage_in_mib: float = 0
+        min_mem_in_gib: float,
+        task_name: Optional[str] = None,
+        estimated_memory_usage_in_gib: Optional[float] = None,
     ):
         """Add a task to the scheduler."""
-        min_mem = min_mem_in_mib * (1 << 20)
-        estimated_memory_usage = estimated_memory_usage_in_mib * (1 << 20)
-        self.tasks.append((cmd_command, min_mem, estimated_memory_usage))
+        min_mem = min_mem_in_gib * (1 << 30)
+        if estimated_memory_usage_in_gib is None:
+            estimated_memory_usage = min_mem
+        else:
+            estimated_memory_usage = estimated_memory_usage_in_gib * (1 << 30)
+        self.tasks.append((cmd_command, min_mem, task_name, estimated_memory_usage))
+
+    @staticmethod
+    def popen_with_callback(on_start, on_exit, popen_kwargs):
+        """
+        Runs the given args in a subprocess.Popen, and then calls the function
+        on_exit when the subprocess completes.
+        on_exit is a callable object, and popen_args is a list/tuple of args that
+        would give to subprocess.Popen.
+        https://stackoverflow.com/questions/2581817/python-subprocess-callback-when-cmd-exits
+        """
+        def run_in_thread(on_start, on_exit, popen_kwargs):
+            proc = subprocess.Popen(**popen_kwargs)
+            on_start(proc)
+            proc.wait()
+            on_exit(proc)
+            return
+        thread = threading.Thread(target=run_in_thread, args=(on_start, on_exit, popen_kwargs))
+        thread.start()
 
     def run(self):
         """Start to run all tasks."""
-        for cmd_command, min_mem, estimated_memory_usage in self.tasks:
+        for cmd_command, min_mem, task_name, estimated_memory_usage in self.tasks:
             devices_list = self._wait_devices(min_mem)
 
-            env = {"CUDA_VISIBLE_DEVICES": ",".join(map(str, devices_list))}
-            print("STARTING ==> CUDA_VISIBLE_DEVICES:", env["CUDA_VISIBLE_DEVICES"])
-            proc = subprocess.Popen(cmd_command, env=env, stdout=subprocess.PIPE)
-            self._set_task_gpu(proc, devices_list, estimated_memory_usage)
+            cuda_visible_devices = ",".join(map(str, devices_list))
+            if not task_name:
+                task_name = datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + "_C" + cuda_visible_devices + "_" + cmd_command[0].split("/")[-1]
+            env = {"CUDA_VISIBLE_DEVICES": cuda_visible_devices}
+            popen_kwargs = {"args": cmd_command, "env": env, "stdout": subprocess.PIPE, "stderr": subprocess.PIPE}
 
+            def get_on_start(task_name, cmd_command, env, devices_list, estimated_memory_usage):
+                def on_start(proc):
+                    self.logger.info(f"STARTING {task_name}_{proc.pid} CUDA_VISIBLE_DEVICES: {env['CUDA_VISIBLE_DEVICES']} {' '.join(cmd_command)}")
+                    self._set_task_gpu(proc, devices_list, task_name, estimated_memory_usage)
+                return on_start
+
+            def on_exit(proc):
+                self._unset_task_gpu(proc)
+
+            self.popen_with_callback(
+                on_start=get_on_start(task_name, cmd_command, env, devices_list, estimated_memory_usage),
+                on_exit=on_exit,
+                popen_kwargs=popen_kwargs,
+            )
+
+        try:
+            self._join_procs()
+        except KeyboardInterrupt:
+            self._wait_when_error()
         self.tasks = []
 
-    def __del__(self):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
         if len(self.tasks) > 0:
-            print("Warning: some processes are not executed. Use run() to execute them.")
-        else:
-            self._wait_procs(True)
+            self.run()
 
 
 if __name__ == '__main__':
 
-    gpu_get = GPUScheduler(min_gpu_count=1, max_gpu_count=2)
+    with GPUScheduler(min_gpu_count=1, max_gpu_count=2) as g:
+        g.schedule(["/usr/bin/python3", "which_gpu_am_i.py"], 15)
+        g.schedule(["/usr/bin/python3", "which_gpu_am_i.py"], 15)
+        g.schedule(["/usr/bin/python3", "which_gpu_am_i.py"], 15)
+        g.schedule(["/usr/bin/python3", "which_gpu_am_i.py"], 15)
 
-    gpu_get.add(["/usr/bin/python3", "which_gpu_am_i.py"], 9000)
-    gpu_get.add(["/usr/bin/python3", "which_gpu_am_i.py"], 9000)
-    gpu_get.add(["/usr/bin/python3", "which_gpu_am_i.py"], 9000)
-    gpu_get.add(["/usr/bin/python3", "which_gpu_am_i.py"], 9000)
-    gpu_get.run()
